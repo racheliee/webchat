bcb3d31e6a2f806b883d7b7ae600e793c20fcce7
racheliee <rachelie.dwg@gmail.com>
2025-02-03 15:57:21 +0900

    fix image domain

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,13 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      }
-    ]
-  }
+    domains: ["avatars.githubusercontent.com"],
+  },
 };
 
 export default nextConfig;
